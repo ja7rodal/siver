e7a87d69df5e68de1f68612324a776b36f3e05d8
Jasson Rodriguez <ja7rodal@gmail.com>
2021-04-25 03:40:18 -0500

    Require in modeles

--- lib/models/driver.rb
+++ lib/models/driver.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'user'
+
 class Driver < User
   has_many :trips
   has_many :transactions, through: :trips
--- lib/models/rider.rb
+++ lib/models/rider.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'user'
+
 class Rider < User
   has_many :payment_sources
   has_many :trips
